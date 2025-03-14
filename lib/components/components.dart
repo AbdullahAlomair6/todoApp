bc21ab0a1070dfeb8e2150c0a000a58321007c85
AbdullahAlomair <abdullah.alomair@globant.com>
2025-03-14 20:05:11 +0300

    Revert "try to delete"

--- lib/components/components.dart
+++ lib/components/components.dart
@@ -1,28 +1,28 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app/cubit.dart';
-
-
-
-
-Widget defaulteFormField({
-  required TextEditingController controller,
-  required TextInputType type,
-  Function(String)? onSubmit,
-  bool isPassword = false,
-  Function(String)? onChanged,
-  Function()? onTap,
-  String? Function(String?)?validator
-
-})=>TextFormField(
-controller: controller ,
-keyboardType: type,
-obscureText: isPassword,
-onFieldSubmitted: onSubmit ,
-onChanged: onChanged,
-onTap: onTap ,
-validator: validator,
-);
+//
+// Widget defaulteFormFild({
+//   required TextEditingController controller,
+//   required TextInputType type,
+//   Function? onSubmit,
+//   bool isPassword = false,
+//
+//
+// })=>TextFormField(
+// controller: controller ,
+// keyboardType: type,
+// obscureText: isPassword,
+// onFieldSubmitted: onSubmit,
+// onChanged: ,
+// onTap: ,
+// validator: ,
+//
+//
+//
+//
+// )
 
 Widget buildTaskItem(Map model, context)=>Padding(
   padding: const EdgeInsets.all(15.0),
